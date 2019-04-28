3922b160fec02c8e247c533c1e41fc4e0c279609
Md. Jaowad Hasan <43891126+Jaowad@users.noreply.github.com>
2019-04-28 12:49:00 +0600

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <div align="center">    
+    <table>    
+        <tr>    
+            <td>EmpId</td>    
+            <td>    
+                <asp:TextBox ID="txtEmpId" runat="server" />    
+            </td>    
+        </tr>    
+        <tr>    
+            <td>EmpName</td>    
+            <td>    
+                <asp:TextBox ID="txtEmpName" runat="server" />    
+            </td>    
+        </tr>    
+        <tr>    
+            <td>EmpSalary</td>    
+            <td>    
+                <asp:TextBox ID="txtEmpSalary" runat="server" />    
+            </td>    
+        </tr>    
+        <tr>    
+            <td>Department</td>    
+            <td>    
+                <asp:TextBox ID="txtDept" runat="server" />    
+            </td>    
+        </tr>    
+    </table>    
+    <br />    
+    <asp:Button ID="btnInsert" runat="server" Text="Insert" Height="30" Width="70" OnClick="btnInsert_Click" OnClientClick="return validate();" />    
+    <br /><br />    
+    <asp:GridView ID="GridView1" HeaderStyle-BackColor="Black" HeaderStyle-ForeColor="White" runat="server" AutoGenerateColumns="false" OnRowEditing="GridView1_RowEditing">    
+        <Columns>    
+            <asp:BoundField DataField="EmpId" HeaderText="EmpId" ReadOnly="true" />    
+            <asp:BoundField DataField="EmpName" HeaderText="EmpName" />    
+            <asp:BoundField DataField="EmpSalary" HeaderText="EmpSalary" />    
+            <asp:BoundField DataField="Dept" HeaderText="Department" />    
+            <asp:TemplateField>    
+                <ItemTemplate>    
+                    <asp:LinkButton ID="LinkButton1" Text="Edit" runat="server" CommandName="Edit" />    
+                    <asp:LinkButton ID="LinkButton2" Text="Delete" runat="server" OnClick="OnDelete" />    
+                </ItemTemplate>    
+                <EditItemTemplate>    
+                    <asp:LinkButton ID="LinkButton3" Text="Update" runat="server" OnClick="OnUpdate" />    
+                    <asp:LinkButton ID="LinkButton4" Text="Cancel" runat="server" OnClick="OnCancel" />    
+                </EditItemTemplate>    
+            </asp:TemplateField>    
+        </Columns>    
+    </asp:GridView>    
+</div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!this.IsPostBack)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("EmpId");
+            dt.Columns.Add("EmpName");
+            dt.Columns.Add("EmpSalary");
+            dt.Columns.Add("Dept");
+            dt.Rows.Add(1, "JACKSON", 75000, "RESEARCH");
+            dt.Rows.Add(2, "JOHNSON", 18000, "ACCOUNTING");
+            dt.Rows.Add(3, "GRANT", 32000, "SALES");
+            dt.Rows.Add(4, "ADAMS", 34000, "OPERATIONS");
+            ViewState["dt"] = dt;
+            BindGrid();
+        } 
+    }
+    protected void BindGrid()
+    {
+        GridView1.DataSource = ViewState["dt"] as DataTable;
+        GridView1.DataBind();
+    }
+    protected void GridView1_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        GridView1.EditIndex = e.NewEditIndex;
+        BindGrid();
+    }
+    protected void OnUpdate(object sender, EventArgs e)
+    {
+        GridViewRow row = (sender as LinkButton).NamingContainer as GridViewRow;
+        string empname = ((TextBox)row.Cells[1].Controls[0]).Text;
+        int empsal = Convert.ToInt32(((TextBox)row.Cells[2].Controls[0]).Text);
+        string dept = ((TextBox)row.Cells[3].Controls[0]).Text;
+
+        DataTable dt = ViewState["dt"] as DataTable;
+        dt.Rows[row.RowIndex]["EmpName"] = empname;
+        dt.Rows[row.RowIndex]["EmpSalary"] = empsal;
+        dt.Rows[row.RowIndex]["Dept"] = dept;
+        ViewState["dt"] = dt;
+        GridView1.EditIndex = -1;
+        BindGrid();
+    }
+    protected void OnCancel(object sender, EventArgs e)
+    {
+        GridView1.EditIndex = -1;
+        BindGrid();
+    }
+    protected void OnDelete(object sender, EventArgs e)
+    {
+        GridViewRow row = (sender as LinkButton).NamingContainer as GridViewRow;
+        DataTable dt = ViewState["dt"] as DataTable;
+        dt.Rows.RemoveAt(row.RowIndex);
+        ViewState["dt"] = dt;
+        BindGrid();
+    }
+    protected void btnInsert_Click(object sender, EventArgs e)
+    {
+        DataTable dt = ViewState["dt"] as DataTable;
+        dt.Rows.Add(txtEmpId.Text, txtEmpName.Text, txtEmpSalary.Text, txtDept.Text);
+        ViewState["dt"] = dt;
+        BindGrid();
+        txtEmpId.Text = string.Empty;
+        txtEmpName.Text = string.Empty;
+        txtEmpSalary.Text = string.Empty;
+        txtDept.Text = string.Empty;
+    } 
+}
